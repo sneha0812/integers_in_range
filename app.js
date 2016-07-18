c6df40030090b6dc5e97bb691b56ba73f1714665
Sneha Agrawal <snehaagrawal@apconic.net>
2016-07-18 15:12:03 +0530

    ugug

---
+++ app.js
@@ -0,0 +1,12 @@
+function integers_in_range(){
+  let value1 = prompt("enter value1");
+  let value2 = prompt("enter value2");
+  if(value1 === 1 && value2 === 1){
+    return 1;
+  }
+  return 
+  for(let i = value1 + 1; i < value2; i++) {
+    console.log(i);
+  }
+}
+integers_in_range();
